a198b88b895a18f61cf10108f07c49e38577328e
dewey8899 <1246818050@qq.com>
2022-05-14 16:34:51 +0800

    负载均衡和服务发现

--- dewey-rpc-all/src/main/java/com/dewey/rpc/rpc/protocol/trpc/TrpcClientInvoker.java
+++ dewey-rpc-all/src/main/java/com/dewey/rpc/rpc/protocol/trpc/TrpcClientInvoker.java
@@ -7,8 +7,8 @@ import com.dewey.rpc.rpc.Response;
 import com.dewey.rpc.rpc.RpcInvocation;
 import com.dewey.rpc.rpc.protocol.trpc.handler.TrpcClientHandler;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
+import java.util.Map;
+import java.util.concurrent.*;
 
 /**
  * @auther dewey
@@ -42,8 +42,69 @@ public class TrpcClientInvoker implements Invoker {
         Response response = (Response) result;
         if (response.getStatus() == 200) {
             return response.getContent();
-        }else {
+        } else {
             throw new Exception("server error:" + response.getContent().toString());
         }
     }
+
+    private static final Map<Long, CompletableFuture> invokerMap = new ConcurrentHashMap<>();
+
+    public static CompletableFuture waitResult(long messageId) {
+        CompletableFuture future = new CompletableFuture();
+        invokerMap.put(messageId, future);
+        return future;
+    }
+    // 处理业务
+    public static void doWhat(Long requestId,Object message) throws InterruptedException {
+        Response response = new Response();
+        response.setContent(message);
+        response.setRequestId(requestId);
+        Thread.sleep(1000);
+        //接受所有的结果  -- 和invoker 调用者不在一个线程
+        //根据id 和 具体的请求对应起来 complete发送结果
+        invokerMap.get(response.getRequestId()).complete(response);
+        invokerMap.remove(requestId);
+    }
+    public static void main(String[] args) throws InterruptedException {
+
+        new Thread(()->{
+            CompletableFuture completableFuture = waitResult(2);
+            //future get 获取结果
+            Object result = null;
+            try {
+                result = completableFuture.get(1, TimeUnit.SECONDS);
+                Response response = (Response) result;
+                System.out.println(response.getContent());
+                System.out.println( "2L -->" + Thread.currentThread().getId());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (TimeoutException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        doWhat(2L,"dewey set response2");
+
+        new Thread(()->{
+            CompletableFuture completableFuture = waitResult(3);
+            //future get 获取结果
+            Object result = null;
+            try {
+                result = completableFuture.get(1, TimeUnit.SECONDS);
+                Response response = (Response) result;
+                System.out.println(response.getContent());
+                System.out.println( "3L -->" + Thread.currentThread().getId());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (TimeoutException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        doWhat(3L,"dewey set response3");
+
+    }
+
 }
